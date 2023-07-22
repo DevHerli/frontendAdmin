199143c769c0a296f34c83827f000cf43dbe7700
Christian Lira <christianlira.dev@gmail.com>
2023-07-21 18:45:22 -0700

    ADD: created globalImagesConst, added logos to sidebar

---
+++ src/app/core/data/consts/globalImages.const.ts
@@ -0,0 +1,6 @@
+export const globalImagesConst = {
+  logos:{
+    comercioLocal_mini_transparente: "assets/images/logos/logoCL_transparente.png",
+    comercioLocal_letras_transparente: "assets/images/logos/CL_letras_transparente.png",
+  }
+}
--- src/app/core/sidebar/sidebar.component.ts
+++ src/app/core/sidebar/sidebar.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
-
+import { globalImagesConst } from '../data/consts/globalImages.const';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent {
+  public globalImages: any = globalImagesConst;
 
-  sidebarActive(){
+  constructor() {}
+
+  sidebarActive() {
     document.getElementsByTagName('body')[0].classList.remove('enlarge-menu');
   }
-
 }
